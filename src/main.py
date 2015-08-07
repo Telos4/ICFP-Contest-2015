37d4fedd9096ba46facd1b938535de0f735d6b5e
spirkelmann <simon.pirkelmann@gmail.com>
2015-08-07 18:10:28 +0200

    added class for units and cells + test in main

--- src/main.py
+++ src/main.py
@@ -1,8 +1,21 @@
 # dummy main
 import lcd_generator as lcd
+import handlejson
+import unit
 
 def main():
     print "ICFP 2015"
+
+    # test JSON parser
+    problem_dict = handlejson.parse_to_dictionary(handlejson.data)
+
+    # create a units
+    units = [unit.Unit(u) for u in problem_dict['units']]
+
+    for u in units:
+        print u
+        print "-------------------------"
+
 
     # test of random sequence generator for the example from the documentation
     unit_indizes = lcd.generate_random_sequence(17,10)
