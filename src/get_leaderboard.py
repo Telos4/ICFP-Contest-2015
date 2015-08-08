2d2df908a925b2eda821f05cd6e5b6d1c5cf5358
Daniel Heinlein <dhein112@gmail.com>
2015-08-08 11:57:06 +0200

    added features to leaderboard

--- src/get_leaderboard.py
+++ src/get_leaderboard.py
@@ -2,6 +2,18 @@ import httplib
 import json
 import time
 
+class color:
+	PURPLE = '\033[95m'
+	CYAN = '\033[96m'
+	DARKCYAN = '\033[36m'
+	BLUE = '\033[94m'
+	GREEN = '\033[92m'
+	YELLOW = '\033[93m'
+	RED = '\033[91m'
+	BOLD = '\033[1m'
+	UNDERLINE = '\033[4m'
+	END = '\033[0m'
+
 oldtime = None
 
 while True:
@@ -26,18 +38,18 @@ while True:
 	globalscore = [ a for a in parsed['data']['rankings'] if 'Wagner' in a['team'] ][0]
 
 	print '\n' * 30
-	print 'date of data:', parsed['time']
-	print 'place in total:', parsed['data']['rankings'].index(globalscore), 'of', len(parsed['data']['rankings'])
+	print color.BOLD + 'date of data:' + color.END, parsed['time']
+	print color.BOLD + 'place in total:' + color.END, parsed['data']['rankings'].index(globalscore), 'of', len(parsed['data']['rankings'])
 	mxrk = max( [ x['rank'] for x in parsed['data']['rankings'] ] )
 	mxscr = max( [ x['score'] for x in parsed['data']['rankings'] ] )
 	mxpwr = max( [ x['power_score'] for x in parsed['data']['rankings'] ] )
-	print 'max\t%s\t%s\t%s' % (mxrk,mxscr,mxpwr)
+	print color.BOLD + 'max' + color.END +'\t%s\t%s\t%s' % (mxrk,mxscr,mxpwr)
 	mn = lambda l: "%.1f" % (float(sum(l))/len(l)) if len(l) > 0 else float('nan')
 	mnrk = mn( [ x['rank'] for x in parsed['data']['rankings'] ] )
 	mnscr = mn( [ x['score'] for x in parsed['data']['rankings'] ] )
 	mnpwr = mn( [ x['power_score'] for x in parsed['data']['rankings'] ] )
-	print 'mean\t%s\t%s\t%s' % (mnrk,mnscr,mnpwr)
-	print 'nr\trank\tscore\tpower_score\ttags'
+	print color.BOLD + 'mean'+color.END+'\t%s\t%s\t%s' % (mnrk,mnscr,mnpwr)
+	print color.BOLD + 'nr\trank\tscore\tpower_score\ttags' + color.END
 	print '-\t%s\t%s\t%s\t-' % (globalscore['rank'],globalscore['score'],globalscore['power_score'])
 
 	for i, a in enumerate(parsed['data']['settings']):
