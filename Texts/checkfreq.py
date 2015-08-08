37ad920f1f584a0a9bd0f4bd6b4e8c96a85123cc
Oleg Lobachev <oleg.lobachev@uni-bayreuth.de>
2015-08-08 12:44:33 +0200

    checkfreq trainwreck

--- Texts/checkfreq.py
+++ Texts/checkfreq.py
@@ -18,6 +18,17 @@ for x, y in pairwise(l):
    print "%d + %d = %d" % (x, y, x + y)
 """
 
+# https://stackoverflow.com/questions/18809482/python-nesting-dictionary-ordereddict-from-collections
+class OrderedDefaultDict(OrderedDict):
+    def __init__(self, default_factory=None, *args, **kwargs):
+        super(OrderedDefaultDict, self).__init__(*args, **kwargs)
+        self.default_factory = default_factory
+    def __missing__(self, key):
+        if self.default_factory is None:
+            raise KeyError(key)
+        val = self[key] = self.default_factory()
+        return val
+
 f = open("words.pickle", 'r')
 othersd = pickle.load(f)
 f.close()
@@ -27,22 +38,29 @@ datf = open("all_hpl.txt", 'r')
 words = datf.read()
 datf.close()
 
+print "files read"
+
 # meh
 # words.decode('utf-8').lower()
 
-ld = OrderedDict
+ld = OrderedDefaultDict(OrderedDict)
 for word in words.split():
     d = defaultdict(int)
     for letter in word:
         d[letter] += 1
     od = OrderedDict(sorted(d.items()))
-    print od
-    # ld[word] = od
+    # print od
+    ld[word] = od
+
+print "categorized all H.P.L. words"
 
 ## have ordered dicts of each word in H.P.L.'s texts
 ## need to compare to baseline (othersd)
 
-tol = 200
+tol = 5000
+ct = 0
+maxt = -9999
+mint = 9999
 
 for word in ld:
     print word
@@ -52,10 +70,20 @@ for word in ld:
         # H.P.L word 'word' with number of these letters in english
         # 'words[letter]'. need to look for inconsistencies in
         # fractions
-        q = words[letter] / ld[word][letter] * 100
+        try:
+            q = words[letter] / ld[word][letter] * 100
+        except:
+            continue
         testd[letter] = int(q)
 
     for l1, l2 in pairwise(testd):
-        if abs(testd[l1] - testd[l2]) > tol:
-            print "YAY", word
+        ct = abs(testd[l1] - testd[l2])
+        """
+        if ct > maxt: maxt = ct
+        if ct < mint: mint = ct
+        """
+        if ct > tol:
+            print "YAY", word, ct
             break
+
+# print "largest tollerance is", maxt, "smallest is", mint
