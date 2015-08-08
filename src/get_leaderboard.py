853756cdf69a005bd74a0dbbc8de7869bdc177af
Daniel Heinlein <dhein112@gmail.com>
2015-08-08 03:56:42 +0200

    updated leaderboard

--- src/get_leaderboard.py
+++ src/get_leaderboard.py
@@ -12,20 +12,16 @@ while True:
 	capped = d[11:]
 	parsed = json.loads(capped)
 
-	print 'time of site:', parsed['time']
+	print '\n' * 30
+	print parsed['time']
+	print 'nr\trank\tscore\tpower_score\ttags'
 
 	globalscore = [ a for a in parsed['data']['rankings'] if 'Wagner' in a['team'] ][0]
-	del globalscore['teamId']
-	del globalscore['team']
-	del globalscore['tags']
-	print 'main ranking:', globalscore
+	print '-\t%s\t%s\t%s' % (globalscore['rank'],globalscore['score'],globalscore['power_score'])
+
 
 	for i, a in enumerate(parsed['data']['settings']):
 		localscore = [ b for b in a['rankings'] if 'Wagner' in b['team'] ][0]
-		del localscore['teamId']
-		del localscore['team']
-		print i, localscore
+		print '%s\t%s\t%s\t%s\t%s' % (i, localscore['rank'], localscore['score'], localscore['power_score'], localscore['tags'])
 
 	time.sleep(300)
-
-	print '\n\n\n\n'
