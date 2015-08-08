8b451a70c9bc5ecbe58d1c41cc3217979cf85933
Daniel Heinlein <dhein112@gmail.com>
2015-08-08 03:18:33 +0200

    leaderboard updated

--- src/get_leaderboard.py
+++ src/get_leaderboard.py
@@ -1,23 +1,30 @@
 import httplib
 import json
+import time
 
-c = httplib.HTTPSConnection('davar.icfpcontest.org')
-c.request('GET','/rankings.js')
-r = c.getresponse()
-d = r.read()
-#print type(d)
-#print d
+while True:
+	c = httplib.HTTPSConnection('davar.icfpcontest.org')
+	c.request('GET','/rankings.js')
+	r = c.getresponse()
+	d = r.read()
+	c.close()
+	#print type(d)
+	#print d
 
-capped = d[11:]
-parsed = json.loads(capped)
+	capped = d[11:]
+	parsed = json.loads(capped)
 
-#print parsed
+	#print parsed
 
-print 'time of site:', parsed['time']
+	print 'time of site:', parsed['time']
 
-print 'main ranking:'
+	print 'main ranking:'
 
-print [ a for a in parsed['data']['rankings'] if 'Wagner' in a['team'] ][0]
+	print [ a for a in parsed['data']['rankings'] if 'Wagner' in a['team'] ][0]
 
-for i, a in enumerate(parsed['data']['settings']):
-	print i, [ b for b in a['rankings'] if 'Wagner' in b['team'] ][0]
+	for i, a in enumerate(parsed['data']['settings']):
+		print i, [ b for b in a['rankings'] if 'Wagner' in b['team'] ][0]
+
+	time.sleep(300)
+
+	print '\n\n\n\n'
