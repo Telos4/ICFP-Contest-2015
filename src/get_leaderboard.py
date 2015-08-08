711ba8ac6e859d9ff61265a4c74f9bffe6f38852
Daniel Heinlein <dhein112@gmail.com>
2015-08-08 03:15:36 +0200

    leadeboard added and pop modified

---
+++ src/get_leaderboard.py
@@ -0,0 +1,23 @@
+import httplib
+import json
+
+c = httplib.HTTPSConnection('davar.icfpcontest.org')
+c.request('GET','/rankings.js')
+r = c.getresponse()
+d = r.read()
+#print type(d)
+#print d
+
+capped = d[11:]
+parsed = json.loads(capped)
+
+#print parsed
+
+print 'time of site:', parsed['time']
+
+print 'main ranking:'
+
+print [ a for a in parsed['data']['rankings'] if 'Wagner' in a['team'] ][0]
+
+for i, a in enumerate(parsed['data']['settings']):
+	print i, [ b for b in a['rankings'] if 'Wagner' in b['team'] ][0]
