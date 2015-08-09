9b2b3824a0367a09b223d19d21e97417cad410e3
Daniel Heinlein <dhein112@gmail.com>
2015-08-09 14:25:02 +0200

    check word added

---
+++ src/check_word.py
@@ -0,0 +1,21 @@
+import data
+import data_structures
+import handlejson
+import convert_class_to_letters
+
+def possibleid(a):
+	mv = []
+	if type(a) == str:
+		mv = convert_class_to_letters.convert_back_letter_to_classes(a)
+	else:
+		mv = a
+	for gameid in range(25):
+		gamedata = data.datas[gameid]
+		bm = data_structures.BoardManager(handlejson.parse_to_dictionary(gamedata))
+		for seedid in range(len(bm.queued_units)):
+			score = bm.calc_board_state(bm.get_initial_board(seedid),mv).move_score
+			if score >= 0:
+				return (gameid,seedid)
+	return None
+
+
