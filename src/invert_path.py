21f5b15b394adb51d7928026d7c6d4a9e32cfd01
Oleg Lobachev <oleg.lobachev@uni-bayreuth.de>
2015-08-09 16:43:11 +0200

    improvements to inverse
    - added right-inverse
    - more checks in test
    - conversion table extended to one-character encoding, but is not
    complete, a conversion from inp to inp2 when using inp2 is still
    needed, but is limited to results of conversion of W and E.

--- src/invert_path.py
+++ src/invert_path.py
@@ -43,14 +43,39 @@ def evaluate_meaning(path):
             rot_counter = 0
     return True
 
+"""
+# both versions of conversion tables
+inp = { 'W' : ['p', '\'',  '!', '.', '0', '3'],
+        'E' : ['b', 'c', 'e', 'f', 'y', '2'],
+        'SW': ['a', 'g', 'h', 'i', 'j', '4'],
+        'SE': ['l', 'm', 'n', 'o', ' ', '5'],
+        'R-': ['d', 'q', 'r', 'v', 'z', '1'],
+        'R+': ['k', 's', 't', 'u', 'w', 'x'],
+        ' ' : ['\t', '\n', '\r', '-']} # let's also ignore '-'
+
+inp2 = { 'W' : ['p', '\'',  '!', '.', '0', '3'],
+        'E' : ['b', 'c', 'e', 'f', 'y', '2'],
+        'T': ['a', 'g', 'h', 'i', 'j', '4'],
+        'Q': ['l', 'm', 'n', 'o', ' ', '5'],
+        'Y': ['d', 'q', 'r', 'v', 'z', '1'],
+        'X': ['k', 's', 't', 'u', 'w', 'x'],
+        ' ' : ['\t', '\n', '\r', '-']} # let's also ignore '-'
+"""
 
 # ' '  is a noop
+# make a universal version that accepts both encodings, may however
+# still need a conversion from `inp` to `inp2` in some parts
 m = { 'W' : ['E', 'SE', ' '],
       'E' : ['W', 'SW', ' '],
       'SW': [' ', 'E'],
       'SE': [' ', 'W'],
       'R-': ['R+', ' ', 'SE', 'SW'],
       'R+': ['R-', ' ', 'SW', 'SE'],
+      # the other version
+      'T':  [' ', 'E'],
+      'Q':  [' ', 'W'],
+      'Y':  ['X', ' ', 'Q', 'T'],
+      'X':  ['Y', ' ', 'T', 'Q'],
       ' ' : [] }
 
 
@@ -86,11 +111,56 @@ def inverseLeft(path):
 
     return res
 
+# very similar to above
+def inverseRight(path):
+    res = []
+    for (i, step) in enumerate(reversed(path)):
+        off = 0
+        found = False
+        while off < len(m[step]):
+            candidate = m[step][off]
+            newres = list(res)
+            newres.append(candidate)
+            # print "trying", res, "+", candidate, "@", m[step]
+            if(evaluate_meaning(newres)):
+                res = newres
+                # print "found", res
+                found = True
+                break
+            else:
+                off += 1
+        if not found:
+            # still have not found a suitable movement
+            print "failed to find a suitable right inverse for", path
+            print "inverted merely",i,"tokens:", res, "for", path[0:i]
+            return None
+    print res, "seems to be a right-inverse of", path
+
+    # need to check that `right-inverse + original path` are still a
+    # valid path
+    if not reduce(lambda x, y: x and y, map(evaluate_meaning, path + res)):
+        print "but the concatenation of them is still not valid!"
+        return None
+
+    return res
+
+
 def test():
-    inverseLeft(['E', 'SW', 'W', 'SW', 'SW',
+    l = inverseLeft(['E', 'SW', 'W', 'SW', 'SW',
     'W', 'SE', 'SW', 'SW', 'W', 'SE', 'E', 'E', 'R-', 'SE', 'SE',
     'SE', 'SE', 'SW', 'E', 'SE', 'SE', 'E', 'R+', 'SW', 'SW', 'SE',
     'R+', 'SW', 'SW', 'SE', 'R+', 'R+', 'E'])
+
+    r = inverseRight(['E', 'SW', 'W', 'SW', 'SW',
+    'W', 'SE', 'SW', 'SW', 'W', 'SE', 'E', 'E', 'R-', 'SE', 'SE',
+    'SE', 'SE', 'SW', 'E', 'SE', 'SE', 'E', 'R+', 'SW', 'SW', 'SE',
+    'R+', 'SW', 'SW', 'SE', 'R+', 'R+', 'E'])
+
+    if l == r:
+        print "left-inverse is the same as right-inverse"
+    if l == reversed(r):
+        print "left-inverse is the same as reversed right-inverse"
+
 
 if __name__ == "__main__":
     test()
