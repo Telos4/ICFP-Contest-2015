f8214213c44fbd17a5cf2534bb80ee26c1556aa4
s1mahoeg <s1mahoeg@gmail.com>
2015-08-09 12:42:08 +0200

    added class SimpleBoard

--- src/pathgeneration.py
+++ src/pathgeneration.py
@@ -8,9 +8,27 @@ import draw
 import Queue
 import random
 import heapq
-try:
-    import cv2
-except ImportError:
-    print "opencv not found"
 import power_words
+import data_structures as ds
 
+class SimpleBoard:
+    def __init__(self, width, height, filledCells):
+        self.width = width
+        self.height = height
+        self.filledCells = filledCells
+
+    def get2DArray(self):
+        # workingBoard: 2D-array to be filled according to self.filledCells
+        workingBoard = []
+        row = []
+
+        for j in range(self.height):
+            for i in range(self.width):
+                row.append(ds.Cell(i,j,False))
+            workingBoard.append(row)
+            row = []
+
+        for cell in self.filledCells:
+            workingBoard[cell.x][cell.y].full = True
+
+        return workingBoard
