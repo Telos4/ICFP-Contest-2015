0a95ec5226016e68c4ec32a5d45c15ac4808fa0c
Oleg Lobachev <oleg.lobachev@uni-bayreuth.de>
2015-08-10 12:24:33 +0200

    beginnings of a main submission file -- TO BE FIXED
    not complete yet, waiting for the merge

--- src/testmain.py
+++ src/testmain.py
@@ -23,6 +23,7 @@ print f,t,m,c,p
 
 j = [ handlejson.parse_to_dictionary(open(i).read()) for i in f ]
 
+"""
 print j
 
 print
@@ -35,7 +36,22 @@ print j[1]
 
 
 # do something
+"""
 
+for map_ in j:
+    # old code:
+    
+    # create a boardmanager
+    boardmanager = BoardManager(j)
+
+
+    boardmanager.path_generation()
+
+    r = boardmanager.calc_board_state(boardmanager.get_initial_board(s), sys.argv[1])
+    print "sim says:",r.move_score
+
+
+## output format
 arr_tuple_pid_seednr_tag_sol=[
 (1,2,'','a'),
 (3,4,'','b')
